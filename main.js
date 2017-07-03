97b53c31ac44722701ca5ad8cde4339b867716c3
monsterrave <benja.glanz@gmail.com>
2017-07-03 20:19:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+//#1
+
 console.log("Starting Javascript...");
+
+//#2
+
 var name = Benjamin Glanz;
+
+//#3
+
 var myAge = 29;
 var averageAge = 30;
 var ageDiff = averageAge - myAge;
+
+//#4
 
 var names = ["Christoph", "Jewe", "Ivan", "Dinni", "Manni", "Harry", "Dani"];
 names.sort();
 console.log(names[0]);
 console.log(names[6]);
 console.log(names);
+
+//#5
 
 var ages = [ 35 , 32 , 28 , 30 , 28, 27, 29];
 while (ages[i]) {
@@ -24,6 +36,8 @@ console.log(name);
 console.log(myAge);
 console.log(names);
 
+//#3
+
 if (myAge > ageDiff) {
 	console.log("I\'m older than the average");
 } else if (myAge === ageDiff) {
@@ -32,3 +46,26 @@ if (myAge > ageDiff) {
 	console.log("I\'m younger than the average");
 }
 
+
+//#6
+var exampleArray = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]
+
+function exampleFunction (exampleArray) {
+	if (exampleArray[] === 0){
+		console.log(exampleArray[]);
+	}
+}
+
+function exampleFuntion1 (exampleArray) {
+	if (exampleArray[] ===100) {
+		console.log(exampleArray[]);
+	}
+}
+
+
+
+//#7
+
+var myColors = ["Red" , "Green" , "White" , "Black"];
+console.log(myColors);
+console.log ("Red" +"//"+"Green"+"//"+"White"+"//"+"Black");
