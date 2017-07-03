996ef685076660f225b7f1cb9cc5103241643bdf
monsterrave <benja.glanz@gmail.com>
2017-07-03 20:18:10 +0200

    Add files via upload
    JS EXAM

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("Starting Javascript...");
+var name = Benjamin Glanz;
+var myAge = 29;
+var averageAge = 30;
+var ageDiff = averageAge - myAge;
+
+var names = ["Christoph", "Jewe", "Ivan", "Dinni", "Manni", "Harry", "Dani"];
+names.sort();
+console.log(names[0]);
+console.log(names[6]);
+console.log(names);
+
+var ages = [ 35 , 32 , 28 , 30 , 28, 27, 29];
+while (ages[i]) {
+	console.log(ages);
+	i++;
+}
+for (ages[i]) {
+	i++;
+	console.log(ages);
+}
+
+console.log(name);
+console.log(myAge);
+console.log(names);
+
+if (myAge > ageDiff) {
+	console.log("I\'m older than the average");
+} else if (myAge === ageDiff) {
+	console.log("I\'m as old as the average");
+} else {
+	console.log("I\'m younger than the average");
+}
+
